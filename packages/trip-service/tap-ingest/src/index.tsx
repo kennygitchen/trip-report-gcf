d4c71e3866166a32fe832e2ed8a989a7afdb394e
Kenny Chen <kc1230@hotmail.com>
2019-03-26 16:17:07 +1100

    * added error response and place holder for processing tap * setup script for local deployment

--- packages/trip-service/tap-ingest/src/index.tsx
+++ packages/trip-service/tap-ingest/src/index.tsx
@@ -1,8 +1,15 @@
 import {Request, Response} from "express";
 
 
-function helloGET(req:Request, res:Response)  {
-    res.send("Hello World");
+function ingest(req:Request, res:Response)  {
+    let tap = req.body.tap;
+    if(!tap) {
+        res.status(400).send('No tap defined!');
+    } else {
+        // process tap
+        console.error(`processing tap: ${tap}`);
+        res.status(200).send('Tap has been processed successfully.');
+    }
 }
 
-export {helloGET}
+export {ingest}
