27f70fda648a80a7ee96a841567b06a1dfdfb95b
Kenny Chen <kc1230@hotmail.com>
2019-04-04 12:18:55 +1100

    * use webpack to package the final deployment

---
+++ packages/trip-service/create-trip/webpack.config.shared.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const GeneratePackageJsonPlugin = require('generate-package-json-webpack-plugin');
+
+const buildProdJson = () => {
+// read pacage json
+  const fs = require("fs");
+  const prodPackageJson = JSON.parse(fs.readFileSync("package.json"));
+  delete prodPackageJson.devDependencies;
+  delete prodPackageJson.scripts;
+  return prodPackageJson;
+}
+
+module.exports = {
+  target: "node",
+  entry: {
+    index: './index.ts'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build'),
+    libraryTarget: 'commonjs'
+  },
+  module: {
+    rules: [
+      {test: /\.(js)$/, use: ['babel-loader'], exclude: /node_modules/},
+      {test: /\.ts?$/, use: ['awesome-typescript-loader']},  // handle ts and tsx typescript files
+    ]
+  },
+  externals: [
+    /^(@google-cloud\/)/i
+  ],
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  plugins: [
+    new CleanWebpackPlugin(['build']),
+    new CopyWebpackPlugin([{from: 'serverless.yml', to: 'serverless.yml', toType: 'file'}]),
+    new GeneratePackageJsonPlugin(buildProdJson(), `${__dirname}/package.json`)
+  ]
+};
